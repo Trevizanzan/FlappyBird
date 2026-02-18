3fc348c4d779e85418b4082e501187075f6c194a
Andrea <andreatrevi91@gmail.com>
2026-02-18 12:05:20 +0100

    added WaitingToStartWindow

---
+++ Assets/Scripts/WaitingToStartWindow.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class WaitingToStartWindow : MonoBehaviour
+{
+    private void Start()
+    {
+        Bird.GetInstance().OnStartedPlaying += WaitingToStartWindow_OnStartedPlaying;
+    }
+
+    private void WaitingToStartWindow_OnStartedPlaying(object sender, EventArgs e)
+    {
+        Hide();
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
